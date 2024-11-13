24b11bed3c43ea6dfcb7739a04345402f202b4d6
Matthew Dixon <m.dixon5@outlook.com>
2024-11-13 12:57:27 +0000

    Add post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
-ALLOWED_HOSTS = ['8000-mdixon5-djangoblog-c5152810gvq.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-mdixon5-djangoblog-c5152810gvq.ws.codeinstitute-ide.net', '.herokuapp.com', '8080-mdixon5-djangoblog-c5152810gvq.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
