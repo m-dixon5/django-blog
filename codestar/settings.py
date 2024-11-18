ee7739b303bf050e48e5fed5257158e32aa45841
Matthew Dixon <m.dixon5@outlook.com>
2024-11-18 11:24:42 +0000

    Fix post_detail.html file name

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
 ALLOWED_HOSTS = ['8000-mdixon5-djangoblog-c5152810gvq.ws.codeinstitute-ide.net', '.herokuapp.com', '8080-mdixon5-djangoblog-c5152810gvq.ws.codeinstitute-ide.net']
 
